0b43da3fb7c089eb5b8dfb03c5021c229fa2e1e6
Roberto Alvarez <robertoatc02@gmail.com>
2014-04-13 12:13:01 -0700

    rewrote stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -9,125 +9,39 @@ var stringifyJSON = function(obj){
   if (typeof obj === 'number' || typeof obj === 'boolean'){
 
     return '' + obj;
-
-  }else if (obj === null){
+  }
+  else if (obj === null){
 
     return 'null';
-
-  }else if (Array.isArray(obj)){
-
-    if (obj.length === 0){
-
-      return '[]';
-
-    }else{
-
-      var str = '[';
-
-      if (obj.length === 1){
-
-        if (Array.isArray(obj[0])){
-
-          return str + stringifyJSON(obj[0]) + ']';
-
-        }else if (typeof obj[0] === 'object'){
-
-          return str + '{' + stringifyJSON(obj[0]) + '}';
-        }
-
-        return typeof obj[0] === 'number' ? str + obj[0] + ']' : str + '"' + obj[0] + '"]';
-      }
-      for (var i = 0; i < obj.length - 1; i++){
-
-        if (typeof obj[i] === 'string'){
-
-          str = str + '"' + obj[i] + '",';
-
-        }else if (typeof obj[i] === 'number'){
-
-          str = str + obj[i] + ',';
-
-        }else if (Array.isArray(obj[i])){
-
-          str = str + stringifyJSON(obj[i]) + ',';
-
-        }else{
-        
-          str = str + stringifyJSON(obj[i]) + ',';
-        }
-      }
-      if (Array.isArray(obj[obj.length - 1])){
-
-        str = str + stringifyJSON(obj[obj.length - 1]);
-        return str + ']';
-
-      }else if (typeof obj[obj.length - 1] === 'object'){
-
-        str = str + stringifyJSON(obj[i]);
-        return str + ']';
-      }
-      return typeof obj[obj.length - 1] === 'number' ? str + obj[obj.length - 1] + ']' : str + '"' + obj[obj.length - 1] + '"]';
-
-    }
-  }else if (typeof obj === 'string'){
+  }
+  else if (typeof obj === 'string'){
 
     return '"' + obj + '"';
+  }
+  else if (Array.isArray(obj)){
 
-  }else if (typeof obj === 'object'){
+    var ans = [];
 
-    var str = [];
-    if (Object.keys(obj).length === 0){
+    for (var i = 0; i < obj.length; i++){
 
-      return '{}';
-
-    }else if (Object.keys(obj).length !== 1){
-
-      for (var key in obj){
-
-        if (typeof obj[key] === 'string'){
-
-          str.push('"' + key + '":"' + obj[key] + '"');
-
-        }else if (typeof obj[key] === 'boolean' || obj[key] === null){
-
-          str.push('"' + key + '":' + obj[key]);
-
-        }else if (typeof obj[key] === 'function' || obj[key] === undefined){
-
-          // Do nothing, ha !
-
-        }else{
-
-          str.push('"' + key + '":' + stringifyJSON(obj[key]));
-
-        }
-
-      }
-
-      return '{' + str.join(',') + '}';
-
-
-    }else{
-
-      if (typeof obj[Object.keys(obj)[0]] === 'string'){
-
-        return '{"' + Object.keys(obj)[0] + '":"' + obj[Object.keys(obj)[0]] + '"}'; 
-
-      }else if (typeof obj[Object.keys(obj)[0]] === 'function' || obj[Object.keys(obj)[0]] === undefined){
-
-        // Nothing here !
-
-      }else{
-
-        return '{"' + Object.keys(obj)[0] + '":' + stringifyJSON(obj[Object.keys(obj)[0]]) + '}'; 
-      }
-
-      return 'troll';
+      ans.push(stringifyJSON(obj[i]));
     }
 
+    return '[' + ans.join(',') + ']';
     
-  }
+  }else{
 
-  
-  //return JSON.stringify(obj);
+    var ans = [];
+
+    for (var key in obj){
+
+      if (typeof obj[key] !== 'function' && obj[key] !== undefined){
+
+        ans.push('"' + key + '":' + stringifyJSON(obj[key]));
+      }
+    }
+
+    return '{' + ans.join(',') + '}';
+
+  }
 };
